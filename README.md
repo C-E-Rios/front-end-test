64b9cf7ddf5a4dcdfebc2d604bb3d18df7e644af
Carlos Gomez Rios <hello@carlosrios.co.uk>
2022-07-28 15:46:57 +0700

    Init project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Illuvium Interview Exercise
+
+Build a web page similar to https://www.illuvium.io/nfts
+
+Data: https://illuvium-interview-test.s3.amazonaws.com/nfts.json
+
+To run the project locally:
+
+### `npm start`
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Illuvium Interview</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
